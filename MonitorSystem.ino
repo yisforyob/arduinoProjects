edfe8e31a5a9d4ef048385194859b8a390d3197d
yisforyob <68566877+yisforyob@users.noreply.github.com>
2023-03-01 13:33:37 -0500

    Update MonitorSystem.ino
    Minor modifications

--- MonitorSystem.ino
+++ MonitorSystem.ino
@@ -1,13 +1,4 @@
 /*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
 
   The circuit:
  * LCD RS pin to digital pin 12
@@ -22,21 +13,6 @@
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- https://docs.arduino.cc/learn/electronics/lcd-displays
 
 */
 
