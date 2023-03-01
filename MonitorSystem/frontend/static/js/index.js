73785998e098ee0c0c1df1c21df76b370a9c64b4
yisforyob <yadngl3@gmail.com>
2023-03-01 17:57:48 -0500

    Working on the web app

---
+++ MonitorSystem/frontend/static/js/index.js
@@ -0,0 +1,52 @@
+import Temperature from "./views/Temperature";
+
+const navigateTo = url =>{
+    history.pushState(null, null, url);
+    router();
+};
+
+const router = async () => {
+    const routes = [
+        {path: "/", view: Temperature},
+        //{path: "/humidity", view: () => console.log("Viewing Humidity")},
+        ///{path: "/airquality", view: () => console.log("Viewing Air Quality")},
+    ];
+
+    //Test each route for potential match 
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        }
+
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    if(!match){
+        match = {
+            route : routes[0],
+            isMatch: true
+        };
+    }
+
+    const view = new match.route.view();
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+
+    console.log(match.route.view());
+
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if(e.target.matches("[data-link]")){
+            e.preventDefault();
+            navigateTo(e.target.href)
+        }
+    });
+
+    router();
+});
---
+++ MonitorSystem/frontend/static/js/views/Temperature.js
@@ -0,0 +1,23 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView{
+    constructor(){
+        super();
+        this.setTitle("Temperature");
+    }
+    async getHtml(){
+        return 
+            <> 
+            <h1> Welcome back, Dear User</h1>
+            <p>
+            Here is the current temperature of the room.
+            </p>
+            <p>
+                <a href="/humidity" data-link> View Humidity</a>.
+            </p>
+            </>
+        ;
+        
+
+    }
+}
